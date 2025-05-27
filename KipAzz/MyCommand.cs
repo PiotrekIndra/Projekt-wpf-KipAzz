161e5134a9702b315d04eaaba0203005bb61ca58
Orozon64 <128033558+Orozon64@users.noreply.github.com>
2025-05-27 15:04:58 +0200

    Add files via upload

---
+++ KipAzz/MyCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace KipAzz
+{
+    public class MyCommand : ICommand
+    {
+        Action _execute;
+
+        public MyCommand(Action executeMethod)
+        {
+            _execute = executeMethod;
+        }
+        public void Execute(object parameter)
+        {
+            if (_execute != null)
+            {
+                _execute();
+            }
+        }
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+        public event EventHandler CanExecuteChanged;
+    }
+}
---
+++ KipAzz/PasswordVM.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KipAzz
+{
+    class PasswordVM : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public MyCommand AddPasword { get; set; }
+        public MyCommand UpdatePassword { get; set; }
+
+        private void OnPropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+    }
+}
